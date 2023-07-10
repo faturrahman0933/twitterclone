ea53d5d55972e0b7b5ef42412f716d6f6e9712f6
faturrahman0933 <faturrahman0933@gmail.com>
2023-07-10 14:10:07 +0700

    login user with appwrite

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -4,7 +4,20 @@ abstract class IAuthAPI {
 
 class AuthAPI implements IAuthAPI {
   @override
-  void signUp() {
-    // TODO: implement signUp
-  }
+  FutureEither<model.Session> login(
+      {required String email, required String password}) async {
+    try {
+      final session = await _account.createEmailSession(
+        email: email,
+        password: password,
+      );
+      return right(session);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occured', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
 }
