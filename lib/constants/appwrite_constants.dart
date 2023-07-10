2b9a060756a98ed7c6478044085e5c48a6280afe
faturrahman0933 <faturrahman0933@gmail.com>
2023-07-10 13:44:42 +0700

    persiapan project

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = 'DATABASE - ID';
+  static const String projectId = 'PROJECT - ID';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
