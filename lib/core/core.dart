54c85aed795afa948aa2f929535c28e96bb986dd
faturrahman0933 <faturrahman0933@gmail.com>
2023-07-10 14:21:37 +0700

    core

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,4 +1,2 @@
-export 'failure.dart';
-export 'type_defs.dart';
-export 'providers.dart';
-export 'utils.dart';
+export './failure.dart';
+export './type_defs.dart';
--- lib/core/providers.dart
+++ lib/core/providers.dart
@@ -6,11 +6,26 @@ final appwriteClientProvider = Provider((ref) {
   Client client = Client();
   return client
       .setEndpoint(AppwriteConstants.endPoint)
-      .setProject(AppwriteConstants.projectId);
-  //.setSelfSigned(true); //if appwrite installed in localhost
+      .setProject(AppwriteConstants.projectId)
+      .setSelfSigned(status: true);
 });
 
 final appwriteAccountProvider = Provider((ref) {
   final client = ref.watch(appwriteClientProvider);
   return Account(client);
+});
+
+final appwriteDatabaseProvider = Provider((ref) {
+  final client = ref.watch(appwriteClientProvider);
+  return Databases(client);
+});
+
+final appwriteStorageProvider = Provider((ref) {
+  final client = ref.watch(appwriteClientProvider);
+  return Storage(client);
+});
+
+final appwriteRealtimeProvider = Provider((ref) {
+  final client = ref.watch(appwriteClientProvider);
+  return Realtime(client);
 });
